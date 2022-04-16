335ce8a8eb5c3aeaea8ffb4ada2abc1979e5fe10
unknown <ajahuanex>
2022-04-16 09:48:42 -0500

    try catch in controllers

--- server/controllers/movies.controllers.js
+++ server/controllers/movies.controllers.js
@@ -1,24 +1,50 @@
 import Movie from "../models/Movie.js";
 
 export const getMovies=async (req, res)=>{
-    const movies = await Movie.find();
-    res.send(movies);
+    try {
+        const movies = await Movie.find();
+        res.send(movies);
+        
+    } catch (error) {
+        return res.status(500).json({message:error.message});
+        
+    }
+   
 };
 
 export const addMovie= async (req, res)=> {
-    const {title, description,year, duration,genre,rating, image} = req.body;
-    const newMovie = new Movie({title, description,year, duration,genre,rating, image});
-    await newMovie.save(); //Guarda en la base de datos
-    console.log(newMovie);
-    return res.send(newMovie)
+    try {
+        const {title, description,year, duration,genre,rating, image} = req.body;
+        const newMovie = new Movie({title, description,year, duration,genre,rating, image});
+        await newMovie.save(); //Guarda en la base de datos
+        console.log(newMovie);
+        return res.send(newMovie)
+        
+    } catch (error) {
+        return res.status(500).json({message:error.message});
+        
+    }
+        
+    
+
 };
 
 export const updateMovie=async (req, res)=>{
-    const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {new: true});
-    console.log(movie);
-    res.send(movie);}
+    try {
+        const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        console.log(movie);
+        res.send(movie);
+    
+    } catch (error) {
+        return res.status(500).json({message:error.message});
+        
+    }
+        
+    
+};
 
 export const deleteMovie= async (req, res)=> {
+try {
     const movie = await Movie.findByIdAndDelete(req.params.id);
     console.log(movie);
     if(!movie){
@@ -26,10 +52,15 @@ export const deleteMovie= async (req, res)=> {
     }
 
     res.status(200).send("Movie deleted");
+} catch (error) {
+    return res.status(500).json({message:error.message});
+    
+}
+    
 };
 
 export const getMovie = async (req, res)=> {
-
+try {
     const movie = await Movie.findById(req.params.id);
     console.log(movie);
     if(!movie){
@@ -37,6 +68,11 @@ export const getMovie = async (req, res)=> {
     }
 
     res.status(200).send(movie);
+} catch (error) {
+    return res.status(500).json({message:error.message});
+    
+}
+
 
 };
 
